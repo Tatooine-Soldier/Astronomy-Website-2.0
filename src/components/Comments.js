aed0ea50212452bc50c09c88099a83f7275aabff
Tatooine-Soldier <58118455+Tatooine-Soldier@users.noreply.github.com>
2021-07-23 10:53:04 +0100

    Add files via upload
    Added email to contact page

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Comments(comment) {
     return (
         
-            comments.map(comment => {return <Comment key={comment.id} comment={comment}/>})
+        <div>""</div>
         
     )
 }
