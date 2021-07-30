0e1bb31f5c4909c49e86d72b6b1fae8b19b1c784
Tatooine-Soldier <58118455+Tatooine-Soldier@users.noreply.github.com>
2021-07-30 12:26:31 +0100

    Add files via upload

--- src/pages/History.js
+++ src/pages/History.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function History() {
     return (
-        <div>
+        <div className="main">
             History
+            <div className="history-main">
+                <div>
+                    <h2>Ancient Astronomy</h2>
+                </div>
+
+            </div>
         </div>
     )
 }
